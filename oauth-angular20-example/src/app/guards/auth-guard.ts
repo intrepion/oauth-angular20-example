04ad1036dbb243565bc3a5aa487b296bb3570bf1
Oliver Forral <intrepion@gmail.com>
2025-07-28 02:24:42 -0600

    Filling out the auth guard.

--- oauth-angular20-example/src/app/guards/auth-guard.ts
+++ oauth-angular20-example/src/app/guards/auth-guard.ts
@@ -1,5 +1,16 @@
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { Auth } from '../services/auth';
 import { CanActivateFn } from '@angular/router';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+export const authGuard: CanActivateFn = () => {
+  const auth = inject(Auth);
+  const router = inject(Router);
+
+  if (auth.isAuthenticated()) {
+    return true;
+  }
+
+  router.navigate(['/login']);
+  return false;
 };
